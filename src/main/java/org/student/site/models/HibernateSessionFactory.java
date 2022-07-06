6d6a6849f69cf289a562acfc8ea4229085a578d9
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-06 03:25:16 +0300

    add default config with HibernateSessionFactory.java and hiddenFieldFilter in WebConfig.java

---
+++ src/main/java/org/student/site/models/HibernateSessionFactory.java
@@ -0,0 +1,27 @@
+package org.student.site.models;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactory() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                //TODO add AnnotatedClasses
+                //configuration.addAnnotatedClass();
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
