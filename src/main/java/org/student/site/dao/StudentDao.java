165a3ba4b5b72d6296bffa0d8d055261861994e3
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-18 20:24:35 +0300

    add into dao functions to get all students, get student by id

--- src/main/java/org/student/site/dao/StudentDao.java
+++ src/main/java/org/student/site/dao/StudentDao.java
@@ -1,4 +1,17 @@
 package org.student.site.dao;
 
+import org.springframework.stereotype.Component;
+import org.student.site.models.HibernateSessionFactory;
+import org.student.site.models.Student;
+
+import java.util.List;
+
+@Component
 public class StudentDao {
+    public List<Student> getAllStudents(){
+        return HibernateSessionFactory.getSessionFactory().openSession().createQuery("FROM Student").list();
+    }
+    public Student getStudentById(int id){
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Student.class, id);
+    }
 }
