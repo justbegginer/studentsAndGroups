d88068aafeccfb81d1c54bb95a1d846e89096f64
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-18 20:25:39 +0300

    add into dao functions to get all groups, get group by id

--- src/main/java/org/student/site/dao/GroupDao.java
+++ src/main/java/org/student/site/dao/GroupDao.java
@@ -1,5 +1,18 @@
 package org.student.site.dao;
 
-public class GroupDao {
+import org.springframework.stereotype.Component;
+import org.student.site.models.Group;
+import org.student.site.models.HibernateSessionFactory;
+import org.student.site.models.Student;
 
+import java.util.List;
+
+@Component
+public class GroupDao {
+    public List<Student> getAllGroups(){
+        return HibernateSessionFactory.getSessionFactory().openSession().createQuery("FROM Group").list();
+    }
+    public Group getGroupById(int id){
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Group.class, id);
+    }
 }
