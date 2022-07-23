bbd8e9acf8ff7402a9ae57810ff0138c411a038e
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-23 19:48:16 +0300

    add functions using for get all students(getAllStudents) or current student(getStudentById)

--- src/main/java/org/student/site/controllers/StudentController.java
+++ src/main/java/org/student/site/controllers/StudentController.java
@@ -1,9 +1,26 @@
 package org.student.site.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.student.site.dao.StudentDao;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
+    @Autowired
+    private StudentDao studentDao;
+    @GetMapping
+    public String getAllStudents(Model model){
+        model.addAttribute("students", studentDao.getAllStudents());
+        return "student/all";
+    }
+    @GetMapping("/{id}")
+    public String getStudentById(@PathVariable("id") int id, Model model){
+        model.addAttribute("student", studentDao.getStudentById(id));
+        return "student/getStudent";
+    }
 }
