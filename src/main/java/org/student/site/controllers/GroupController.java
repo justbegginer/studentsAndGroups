e219b9a35cd8f191775a4b2f34060e6693bc3d0f
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-23 19:46:57 +0300

    add functions using for get all groups(getAllGroups) or current group(getGroupById)

--- src/main/java/org/student/site/controllers/GroupController.java
+++ src/main/java/org/student/site/controllers/GroupController.java
@@ -1,9 +1,26 @@
 package org.student.site.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.student.site.dao.GroupDao;
 
 @Controller
 @RequestMapping("/groups")
 public class GroupController {
+    @Autowired
+    private GroupDao groupDao;
+    @GetMapping
+    public String getAllGroups(Model model){
+        model.addAttribute("groups", groupDao.getAllGroups());
+        return "group/all";
+    }
+    @GetMapping("/{id}")
+    public String getGroupById(@PathVariable("id") int id, Model model){
+        model.addAttribute("group", groupDao.getGroupById(id));
+        return "group/getGroup";
+    }
 }
