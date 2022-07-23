3f5b1c24e7d99c1fab1697fb340267f757fb75a7
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-23 19:49:20 +0300

    add functions using for get all tutor(getAllTutors) or current tutor(getTutorById)

--- src/main/java/org/student/site/controllers/TutorController.java
+++ src/main/java/org/student/site/controllers/TutorController.java
@@ -1,10 +1,26 @@
 package org.student.site.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.student.site.dao.TutorDao;
 
 @Controller
 @RequestMapping("/tutors")
 public class TutorController {
-
+    @Autowired
+    public TutorDao tutorDao;
+    @GetMapping
+    public String getAllTutors(Model model){
+        model.addAttribute("tutors", tutorDao.getAllTutors());
+        return "/tutor/all";
+    }
+    @GetMapping("/{id}")
+    public String getTutorById(@PathVariable("id") int id, Model model){
+        model.addAttribute("tutor", tutorDao.getTutorById(id));
+        return "tutor/getTutor";
+    }
 }
