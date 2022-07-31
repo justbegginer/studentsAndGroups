30845a77d301104e28ac39e75ecd0770f780a2e5
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-01 02:36:58 +0300

    add controller for all others mapping not related with groups/students/tutors

---
+++ src/main/java/org/student/site/controllers/MainController.java
@@ -0,0 +1,39 @@
+package org.student.site.controllers;
+
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.student.site.dao.GroupDao;
+import org.student.site.dao.StudentDao;
+import org.student.site.dao.TutorDao;
+
+@Controller
+public class MainController {
+    @Autowired
+    private GroupDao groupDao;
+    @Autowired
+    private StudentDao studentDao;
+    @Autowired
+    private TutorDao tutorDao;
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam("string") String string){
+        try{
+            int id = Integer.parseInt(string);
+            model.addAttribute("groups", groupDao.getGroupById(id));
+            model.addAttribute("students", studentDao.getStudentById(id));
+            model.addAttribute("tutors", tutorDao.getTutorById(id));
+            model.addAttribute("string", id);
+        }
+        catch (NumberFormatException exception){
+            model.addAttribute("students", studentDao.searchByString(string));
+            model.addAttribute("tutors", tutorDao.searchByString(string));
+            model.addAttribute("string", string);
+        }
+        return "search";
+    }
+
+}
