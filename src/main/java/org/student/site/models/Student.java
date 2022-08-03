739a006ef1aaa0b66e838f64e160c8edae307b0b
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-04 00:53:37 +0300

    update validation using pattern contains only non-capital letters

--- src/main/java/org/student/site/models/Student.java
+++ src/main/java/org/student/site/models/Student.java
@@ -19,9 +19,11 @@ public class Student {
     private int id;
     @NotEmpty(message = "student name should not be empty")
     @Size(min = 2, max = 30, message = "student name should be between 2 and 30")
+    @Pattern(regexp = "^[a-z]*$", message = "student name should contains only letters")
     private String name;
     @NotEmpty(message = "student surname should not be empty")
     @Size(min = 2, max = 30, message = "student surname should be between 2 and 30")
+    @Pattern(regexp = "^[a-z]*$", message = "student surname should contains only letters")
     private String surname;
     @NotNull(message = "student groupNumber should not be empty")
     @Min(value = 1, message = "student groupNumber should be greater than zero")
--- src/main/java/org/student/site/models/Tutor.java
+++ src/main/java/org/student/site/models/Tutor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -19,11 +20,14 @@ public class Tutor {
     private int id;
     @NotEmpty(message = "tutor name should not be empty")
     @Size(min = 2, max = 30, message = "tutor name should be between 2 and 30")
+    @Pattern(regexp = "^[a-z]*$", message = "tutor name should contains only letters")
     private String name;
     @NotEmpty(message = "tutor surname should not be empty")
     @Size(min = 2, max = 30, message = "tutor surname should be between 2 and 30")
+    @Pattern(regexp = "^[a-z]*$", message = "tutor surname should contains only letters")
     private String surname;
     @NotEmpty(message = "tutor qualification should not be empty")
     @Size(min = 2, max = 30, message = "tutor qualification should be between 2 and 30")
+    @Pattern(regexp = "^[a-z]*$", message = "tutor qualification should contains only letters")
     private String qualification;
 }
